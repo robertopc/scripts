8b43260073b47c97a6531eed532242607b61ee85
robertopc <robertopc.prog@gmail.com>
2017-12-17 01:13:08 -0200

    Update script

--- lubuntu-start.sh
+++ lubuntu-start.sh
@@ -4,8 +4,7 @@
 # It install and configure my main softwares and configs
 
 USER=`whoami`
-
-sudo apt-get install git $PARAMS -qq
+HOME="/home/$USER"
 
 cd ~
 mkdir github
@@ -13,75 +12,82 @@ cd github
 git clone git@github.com:/robertopc/dotfiles
 git clone git@github.com:/robertopc/scripts
 
+ln -s $HOME/github/dotfiles ~
+ln -s $HOME/github/scripts ~
+
 cd ~
+
 # Download fonts backup
 git clone git@github.com:/robertopc/fontes
 
 # Link Dotfiles
-find github/dotfiles/ -type f -name .* -exec ln -s {} \; # create symlinks
+find $HOME/github/dotfiles/ -type f -name .* -exec ln -s {} ~ \; # mk symlinks
 
 # Link Fonts
 ln -s fontes .fonts
 
-# Install parameters
-PARAMS='-y -qq >> lubuntu-start-errors.log'
-
 # Cli apps
 echo "Installing CLI apps..."
+sudo apt-get install pulseaudio -y -qq		# audio
+sudo apt-get install htop -y -qq		# cli top improved
+sudo apt-get install vim -y -qq			# cli vi improved
+sudo apt-get install mutt -y -qq		# cli email client
+sudo apt-get install w3m -y -qq			# cli web browser
+sudo apt-get install cmus -y -qq	  	# cli music player
+sudo apt-get install mpv -y -qq			# cli media player
+sudo apt-get install gist -y -qq		# cli gist
+sudo apt-get install espeak -y -qq		# cli audio synthetizer
+sudo apt-get install httpie -y -qq		# CLI, cURL-like tool for humans
+sudo apt-get install multitail -y -qq		# clie multi tail
+sudo apt-get install inxi -y -qq		# show hardware information
+sudo apt-get install rar -y -qq			# rar
 
-sudo apt-get install pulseaudio $PARAMS		# audio
-sudo apt-get install htop $PARAMS		# top improved
-sudo apt-get install vim $PARAMS		# vi improved
-sudo apt-get install gparted $PARAMS		# disk partitions editor
-sudo apt-get install vlc $PARAMS		# media player
-sudo apt-get install cheese $PARAMS		# webcam
-sudo apt-get install firefox $PARAMS		# web browser
-sudo apt-get install chromium-browser $PARAMS	# web browser
-sudo apt-get install brave $PARAMS		# web browser
-sudo apt-get install w3m $PARAMS		# cli web browser
-sudo apt-get install gimp $PARAMS		# image editor
-sudo apt-get install sqliteman $PARAMS		# SQLite
-sudo apt-get install playonlinux $PARAMS	# wine
-sudo apt-get install inkscape $PARAMS		# vector images editor
-sudo apt-get install kdenlive $PARAMS	        # video editor
-sudo apt-get install kazam $PARAMS		# screencast
-sudo apt-get install rhythmbox $PARAMS	        # music player
-sudo apt-get install audacity $PARAMS	        # sound editor
-sudo apt-get install bleachbit $PARAMS		# file cleaner
-sudo apt-get install indicator-multiload $PARAMS # info about memory, cpu
-sudo apt-get install gedit $PARAMS		# gnome text editor
-sudo apt-get install virtualbox $PARAMS	        # virtual machines
-sudo apt-get install pgadmin3 $PARAMS		# PostgreSQL
-sudo apt-get install terminator $PARAMS		# terminal emulator
-sudo apt-get install typecatcher $PARAMS	# search google fonts
-sudo apt-get install clipit $PARAMS		# clipboard
-sudo apt-get install transmission $PARAMS	# torrent
-sudo apt-get install keepassx $PARAMS		# password manager
-sudo apt-get install apache2 $PARAMS		# http server
-sudo apt-get install php7.0 $PARAMS		# php7
-sudo apt-get install php7.0-cli $PARAMS		# php7 for cli
-sudo apt-get install php7.0-curl $PARAMS	# php7 curl lib
-sudo apt-get install mysql-server $PARAMS	# mysql
-sudo apt-get install gnome-terminal $PARAMS	# steam requeriment
-sudo apt-get install shutter $PARAMS		# screenshot
-sudo apt-get install screenruler $PARAMS	# screen ruler(for design)
-sudo apt-get install mutt $PARAMS		# email client
-sudo apt-get install mediainfo $PARAMS		# show media files info
-sudo apt-get install mediainfo-gui $PARAMS	# media-info graphical
-sudo apt-get install screenkey $PARAMS		# screencast keys
-sudo apt-get install vokoscreen $PARAMS		# screencast
-sudo apt-get install font-manager $PARAMS	# font manager
-sudo apt-get install multitail $PARAMS		# multi tail in cli
-sudo apt-get install rar $PARAMS		# rar
-sudo apt-get install inxi $PARAMS		# show hardware information
-sudo apt-get install nodejs $PARAMS		# nodejs
-sudo apt-get install npm $PARAMS		# nodejs package manager
-sudo apt-get install python3-pip $PARAMS	# python3 package manager
-sudo apt-get install espeak $PARAMS		# audio synthetizer
-sudo apt-get install cmus $PARAMS		# cli music player
-sudo apt-get install mpv $PARAMS		# cli media player
-sudo apt-get install gist $PARAMS		# cli gist
-sudo apt-get install albert $PARAMS		# app launcher
+echo "Installing Browsers..."
+sudo apt-get install firefox -y -qq		# web browser
+sudo apt-get install chromium-browser -y -qq	# web browser
+sudo apt-get install brave -y -qq		# web browser
+
+echo "Installing Media Editors..."
+sudo apt-get install inkscape -y -qq		# vector images editor
+sudo apt-get install gimp -y -qq		# image editor
+sudo apt-get install kdenlive -y -qq	        # video editor
+sudo apt-get install audacity -y -qq	        # sound editor
+
+echo "Installing Programing Tools..."
+sudo apt-get install sqliteman -y -qq	  	# SQLite
+sudo apt-get install pgadmin3 -y -qq		# PostgreSQL
+sudo apt-get install apache2 -y -qq		# http server
+sudo apt-get install php7.0 -y -qq		# php7
+sudo apt-get install php7.0-cli -y -qq		# php7 for cli
+sudo apt-get install php7.0-curl -y -qq		# php7 curl lib
+sudo apt-get install mysql-server -y -qq	# mysql
+sudo apt-get install nodejs -y -qq		# nodejs
+sudo apt-get install npm -y -qq		        # nodejs package manager
+sudo apt-get install python3-pip -y -qq		# python3 package manager
+
+sudo apt-get install cheese -y -qq		# webcam
+sudo apt-get install gparted -y -qq		# disk partitions editor
+sudo apt-get install vlc -y -qq			# media player
+sudo apt-get install playonlinux -y -qq	        # wine
+sudo apt-get install kazam -y -qq		# screencast
+sudo apt-get install rhythmbox -y -qq	        # music player
+sudo apt-get install bleachbit -y -qq		# file cleaner
+sudo apt-get install indicator-multiload -y -qq # info about memory, cpu
+sudo apt-get install gedit -y -qq		# gnome text editor
+sudo apt-get install virtualbox -y -qq	        # virtual machines
+sudo apt-get install terminator -y -qq		# terminal emulator
+sudo apt-get install typecatcher -y -qq		# search google fonts
+sudo apt-get install clipit -y -qq		# clipboard
+sudo apt-get install transmission -y -qq	# torrent
+sudo apt-get install keepassx -y -qq		# password manager
+sudo apt-get install gnome-terminal -y -qq	# steam requeriment
+sudo apt-get install shutter -y -qq		# screenshot
+sudo apt-get install screenruler -y -qq		# screen ruler(for design)
+sudo apt-get install mediainfo -y -qq		# show media files info
+sudo apt-get install mediainfo-gui -y -qq	# media-info graphical
+sudo apt-get install screenkey -y -qq		# screencast keys
+sudo apt-get install vokoscreen -y -qq		# screencast
+sudo apt-get install font-manager -y -qq	# font manager
 
 # Cerebro App Launcher
 wget https://github.com/KELiON/cerebro/releases/download/v0.3.1/cerebro_0.3.1_amd64.deb
