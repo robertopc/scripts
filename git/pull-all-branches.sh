5a1f0bcfd9607b4e5a6c9d9ebcd62c049d3efe93
robertopc <robertopc.prog@gmail.com>
2022-07-02 21:44:18 -0300

    updated

--- git/pull-all-branches.sh
+++ git/pull-all-branches.sh
@@ -1,3 +1,4 @@
+# extracted from https://stackoverflow.com/a/10312587/3329571
 git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 git fetch --all
 git pull --all
