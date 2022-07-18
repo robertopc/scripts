8b56168e5e690e418cea4f0f8712fe9f25e46577
robertopc <robertopc.prog@gmail.com>
2022-07-18 17:47:20 -0300

    added path to git pull all branches

--- git/pull-all-branches.sh
+++ git/pull-all-branches.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# if passed path
+if [ ! -z "$1" ]; then
+    cd $1
+fi
 # extracted from https://stackoverflow.com/a/10312587/3329571
 git branch -r | grep -v '\->' | sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done
 git fetch --all
